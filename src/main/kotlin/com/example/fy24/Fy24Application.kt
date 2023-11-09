2610416d2ac5d32acfe1fe593f5534126911d7e6
fenton <1441281543@qq.com>
2023-11-09 21:14:01 +0800

    first commit

---
+++ src/main/kotlin/com/example/fy24/Fy24Application.kt
@@ -0,0 +1,11 @@
+package com.example.fy24
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Fy24Application
+
+fun main(args: Array<String>) {
+	runApplication<Fy24Application>(*args)
+}
---
+++ src/test/kotlin/com/example/fy24/Fy24ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.fy24
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Fy24ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
